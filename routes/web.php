7f9dab738482d72161379beb3542ed42aa288f72
Nnded <chuabernard0527@gmail.com>
2026-04-27 11:09:06 +0800

    feat(routes): add login and register routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 Route::fallback(fn () => redirect()->route('auth.login'));
+Route::name('auth.')->controller(AuthController::class)->group(function () {
+    Route::get('/login', 'showLogin')->name('login');
+    Route::get('/register', 'showRegister')->name('register');
+    Route::post('/login', 'login')->name('submit-login');
+    Route::post('/register', 'register')->name('submit-register');
+Route::name('app.')->middleware('auth')->group(function () {
+    Route::get('/homepage', fn () => view('app.homepage'))->name('homepage');
+});
+Route::name('admin.')->middleware('auth')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'showDashboard'])->name('dashboard');
 });
