3762fbd4bd62b1ff1c9a74943b2a4890de1e9462
Nnded <chuabernard0527@gmail.com>
2026-04-28 00:19:05 +0800

    fix(dashboard): remove navbar from dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,11 +8,5 @@
     <button>Logout</button>
 </form>
 
-<div class='d-flex flex-column'>
-    <a href='{{ route('admin.dashboard') }}'>Dashboard</a>
-    <a href='{{ route('admin.products.index') }}'>Product Management</a>
-    <a href='{{ route('admin.orders.index') }}'>Order Management</a>
-    <a href='{{ route('admin.users.index') }}'>User Management</a>
-</div>
 @endsection
 
