abbd4f6ec6103ab5a337fe461ed873e76c2cbce4
Nnded <chuabernard0527@gmail.com>
2026-04-27 12:28:53 +0800

    feat(admin panel): add navigation

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,5 +7,12 @@
 <form method='POST' action='{{ route('auth.logout') }}'>
     <button>Logout</button>
 </form>
+
+<div class='d-flex flex-column'>
+    <a href='{{ route('admin.dashboard') }}'>Dashboard</a>
+    <a href='{{ route('admin.products.index') }}'>Product Management</a>
+    <a href='{{ route('admin.orders.index') }}'>Order Management</a>
+    <a href='{{ route('admin.users.index') }}'>User Management</a>
+</div>
 @endsection
 
