5ff4fcb4dbe5100ff6b83f62ebf801cdaae43c92
Nnded <chuabernard0527@gmail.com>
2026-04-27 11:14:18 +0800

    feat(auth controller): add register logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
     public function showLogin()
     {
         return view('auth.login');
+    }
+
+    public function showRegister()
+    {
+        return view('auth.register');
     }
 
     public function login(LoginRequest $request)
@@ -20,5 +34,27 @@ class AuthController extends Controller
 
         $route = Auth::user()->role === 'admin' ? 'admin.dashboard' : 'app.homepage';
         return redirect()->route($route);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+
+        User::create([
+            'first_name' => $validated['first_name'],
+            'last_name'  => $validated['last_name'],
+            'email'      => $validated['email'],
+            'password'   => Hash::make($validated['password']),
+        ]);
+
+        $registered = [
+            'email'    => $validated['email'],
+            'password' => $validated['password'],
+        ];
+
+        Auth::attempt($registered);
+
+        return redirect()->route('app.homepage');
+
     }
 }
