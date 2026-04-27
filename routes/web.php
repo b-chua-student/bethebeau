0db0814e6478455ce132b30d7fac64721c202706
Nnded <chuabernard0527@gmail.com>
2026-04-28 00:20:17 +0800

    feat(routes): import controllers to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 
 Route::fallback(fn () => redirect()->route('auth.login'));
 
