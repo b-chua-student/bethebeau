d48e6a7d34a0af7812fde51e1e2154cdd26d1f9d
Nnded <chuabernard0527@gmail.com>
2026-04-28 00:17:41 +0800

    feat(component): add admin navbar

---
+++ resources/views/components/admin-navbar.blade.php
@@ -0,0 +1,6 @@
+<div class='d-flex flex-column'>
+    <a href='{{ route('admin.dashboard') }}'>Dashboard</a>
+    <a href='{{ route('admin.products.index') }}'>Product Management</a>
+    <a href='{{ route('admin.orders.index') }}'>Order Management</a>
+    <a href='{{ route('admin.users.index') }}'>User Management</a>
+</div>
