5da3f507fa70c781a7b09cfa5c7b23157e93094b
Nnded <chuabernard0527@gmail.com>
2026-04-27 11:16:01 +0800

    feat(routes): add login as guest route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 Route::fallback(fn () => redirect()->route('auth.login'));
+
 Route::name('auth.')->controller(AuthController::class)->group(function () {
     Route::get('/login', 'showLogin')->name('login');
     Route::get('/register', 'showRegister')->name('register');
     Route::post('/login', 'login')->name('submit-login');
     Route::post('/register', 'register')->name('submit-register');
     Route::post('/logout', 'logout')->name('logout');
+    Route::get('/login-guest', 'loginAsGuest')->name('guest');
+});
+
 Route::name('app.')->middleware('auth')->group(function () {
     Route::get('/homepage', fn () => view('app.homepage'))->name('homepage');
 });
