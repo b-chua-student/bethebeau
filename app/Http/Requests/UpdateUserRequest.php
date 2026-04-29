6efd3a230559ccf17aaeddf7780193e2ae5a12a3
Nnded <chuabernard0527@gmail.com>
2026-04-29 20:48:40 +0800

    feat(form request): add data validation for updating user details

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userId = $this->route('user') instanceof User ? $this->route('user')->id : $this->route('user');
+
+        return [
+        'first_name'        => ['required', 'string'],
+        'last_name'         => ['required', 'string'],
+        'email'             => [
+            'required',
+            'email',
+            Rule::unique('users')->ignore($userId)
+        ],
+        'password'          => ['nullable', 'min:8'],
+        'instagram_account' => [
+            'nullable',
+            'string',
+            Rule::unique('users')->ignore($userId)
+        ],
+        'address'           => ['nullable', 'string'],
+        'role'              => ['required', 'in:user,guest,admin'],
+        ];
+    }
+}
