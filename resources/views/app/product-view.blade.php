5a38542a8e56acebdcccd2d998ab7c4ed5c8061d
Nnded <chuabernard0527@gmail.com>
2026-04-30 20:35:24 +0800

    feat(product view): implement add to cart functionality

--- resources/views/app/product-view.blade.php
+++ resources/views/app/product-view.blade.php
@@ -15,5 +15,23 @@
     <li>{{ $product->stock }}</li>
 </ul>
 
+<form method='POST' action='{{ route('app.add-to-cart') }}'>
+    @csrf
+    <input type="hidden" name="product_id" value="{{ $product->id }}"/>
+    <button type="button" onclick="decrement()">-</button>
+    <input type="number" name="quantity" value="1" min="1" id="quantity"/>
+    <button type="button" onclick="increment()">+</button>
+    <button type="submit">Add to Cart</button>
+</form>
+
+<script>
+function increment() {
+    document.getElementById('quantity').stepUp();
+}
+function decrement() {
+    document.getElementById('quantity').stepDown();
+}
+</script>
+
 @endsection
 
