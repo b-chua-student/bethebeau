b4e4150bb03f18568a0fcf8088eba53482411a6b
Nnded <chuabernard0527@gmail.com>
2026-04-30 16:27:17 +0800

    fix(search form component): remove pass route through request

--- resources/views/components/search-form.blade.php
+++ resources/views/components/search-form.blade.php
@@ -1,5 +1,4 @@
 <form action="{{ route('search.' . $route) }}" method="GET">
     <input type="text" name="query" placeholder="Search..." value="{{ request('query') }}"/>
-    <input type="hidden" name="from" value="app.product-listing"/>
     <button type="submit">Search</button>
 </form>
