77fea68d611a205223040aba45d4ff00aea4f017
b-chua-student <s2302677@usls.edu.ph>
2026-04-30 20:38:32 +0800

    Feat/add shopping cart (#5)
    * feat(controller): add shopping cart logic for show, add, and remove
    
    * feat: add cart model
    
    * feat: add cart item model
    
    * feat(product view): implement add to cart functionality
    
    * feat(shopping cart): display shopping cart items
    
    * feat(routes): add index method for returning shopping cart view
    
    * feat: add route for adding items to shopping cart
    
    * feat: add route for removing items from cart
    
    ---------
    
    Co-authored-by: Nnded <chuabernard0527@gmail.com>

--- resources/views/app/shopping-cart.blade.php
+++ resources/views/app/shopping-cart.blade.php
@@ -4,6 +4,34 @@
 
 @section('content')
 <h1>Shopping Cart</h1>
+
+<table>
+    <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Subtotal</th>
+        <th>Action</th>
+    </tr>
+    @foreach ($items as $item)
+    <tr>
+        <td>{{ $item->product->name }}</td>
+        <td>{{ $item->product->price }}</td>
+        <td>{{ $item->quantity }}</td>
+        <td>{{ $item->product->price * $item->quantity }}</td>
+        <td>
+            <form action="{{ route('app.remove-from-cart', $item->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Remove</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+<p>Total: {{ $items->sum(fn($item) => $item->product->price * $item->quantity) }}</p>
+
 <form method='POST' action='{{ route('app.checkout') }}'>
     <button>Checkout</button>
 </form>
