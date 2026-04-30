eb0a7bb6640104afc4df74babad54b570406f2ab
Nnded <chuabernard0527@gmail.com>
2026-04-30 15:19:18 +0800

    feat(search controller): add search product functionality

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\User;
+
+class SearchController extends Controller
+{
+    public function searchProduct(Request $request)
+    {
+        $query = $request->input('query');
+        $from = $request->input('from', 'app.product-listing');
+        $products = Product::search($query)
+            ->query(fn($q) => $q->where('is_active', true))
+            ->get();
+        return view('app.product-listing', compact('products'));
+    }
+}
