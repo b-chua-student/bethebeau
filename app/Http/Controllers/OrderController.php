b853addbf4e60924b32fd2fd6c7a76b2d3d8b38e
Nnded <chuabernard0527@gmail.com>
2026-04-30 19:48:47 +0800

    feat(order management panel): add order items to index view

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,7 +10,7 @@ class OrderController extends Controller
 {
     public function index()
     {
-        $orders = Order::with('user')->latest()->get();
+        $orders = Order::with('user', 'items.product')->latest()->get();
         return view('admin.orders.index', compact('orders'));
     }
 
