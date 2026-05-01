b4e7820761e795ee425e53ad49fb72496655b1af
b-chua-student <s2302677@usls.edu.ph>
2026-05-01 17:58:25 +0800

    feat(app navbar component): add shopping cart icon (#17)
    Co-authored-by: Nnded <chuabernard0527@gmail.com>

--- resources/views/components/app-navbar.blade.php
+++ resources/views/components/app-navbar.blade.php
@@ -34,8 +34,10 @@
         </a>
 
         <!-- Cart -->
-        <a href="{{ route('app.shopping-cart') }}" class="block">
-            <img src="{{ asset('img/cart-white.svg') }}" alt="Cart" class="h-7 w-7 transition-transform duration-200 hover:scale-115">
+        <a href="{{ route('app.shopping-cart') }}" class="group block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 text-white transition-transform duration-200 group-hover:scale-115">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
         </a>
 
         <!-- Logout -->
