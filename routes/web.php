29a8b896513a775531f5604f0638496405327b8f
Nnded <chuabernard0527@gmail.com>
2026-05-01 08:45:48 +0800

    feat: add route to get edit profile view

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::name('app.')->middleware('auth')->group(function () {
 
     Route::name('profile.')->prefix('profile')->group(function () {
         Route::get('/', [ProfileController::class, 'index'])->name('index');
+        Route::get('/edit', [ProfileController::class, 'edit'])->name('edit');
     });
 
 });
