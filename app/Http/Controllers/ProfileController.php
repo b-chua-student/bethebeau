6f6c43d0f0c8bb93d8083c1dd7a989233c98c51e
Nnded <chuabernard0527@gmail.com>
2026-05-01 08:46:59 +0800

    feat(profile controller): add method to update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Requests\UpdateProfileRequest;
 
 class ProfileController extends Controller
 {
@@ -16,5 +19,15 @@ class ProfileController extends Controller
     {
         $user = Auth::user();
         return view('app.profile.edit', compact('user'));
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $user = Auth::user();
+
+        $validated = $request->validated();
+
+        $user->update($validated);
+        return redirect()->route('app.profile.index')->with('success', 'Profile updated.');
     }
 }
