c11acee5cd1052dfdb28185791caa38067d568a5
Nnded <chuabernard0527@gmail.com>
2026-05-01 15:46:12 +0800

    fix(auth layout): remove bootstrap links

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="description" content="" />
   <link rel="icon" href="favicon.png">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="{{ asset('css/components/style.css') }}">
   @vite(['resources/css/app.css'])
 </head>
@@ -14,7 +13,6 @@
 
     @yield('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" />
 </body>
 </html>
 
